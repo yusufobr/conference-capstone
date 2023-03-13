fab7970bc3de0ea47c488fe741723dab2188c8a8
yusufobr <yousarsif80@gmail.com>
2023-03-13 18:05:44 +0100

    added the Hero & the Main Program sections

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      backgroundImage: {
+        bgHero: 'url(../images/bg-hero.svg)',
+        bgProgram: 'url(../images/program-bg.svg)',
+      },
+      colors: {
+        primary: '#272a31',
+        secondary: '#d3d3d3',
+        tGray: '#3e3e3e',
+        callAction: '#ec5242',
+      },
+    },
+  },
+  plugins: [],
+};
